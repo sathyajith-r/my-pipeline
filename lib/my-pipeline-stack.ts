1ce371ffe90dbaac1bcbb5a175fa4ad797c877c8
sathyajith-r <sathyajith.r@ymedialabs.com>
2023-01-24 01:29:29 +0530

    added repo details

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -9,7 +9,7 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('AmwayACS/creators-bff-pipelines-poc', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
